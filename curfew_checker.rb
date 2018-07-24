964ae5e2c257e9b2ee1c348c9cbdc30f9d52a888
sonyapieklik <piesonya@gmail.com>
2018-07-24 16:17:26 +0000

    Done.

--- curfew_checker.rb
+++ curfew_checker.rb
@@ -1,3 +1,4 @@
+
 def simple_curfew_checker(time)
   if time == 11
     return "You're in trouble!"
@@ -9,16 +10,16 @@ def simple_curfew_checker(time)
     return nil
   end
 end
-
+    
 def curfew_checker(time)
- if time == 12
+  if time == 12
     return "You're in trouble!"
   end
   else time == 9
     return "Keep having fun!"
   end
 end
-
+  
 def complex_curfew_checker(time)
   if time == 12
     return "You're in trouble!"
@@ -31,12 +32,8 @@ def complex_curfew_checker(time)
   end
 end
 
-def deluxe_curfew_checker(time)
-  # code goes here
-end
-
-def platinum_curfew_checker(current_time, curfew_time)
-   if time < curfew
+def platnium_curfew_checker(time, curfew)
+    if time < curfew
       hour = curfew - time 
       return "You have #{hour} hour(s) left to keep having fun!"
     end
@@ -47,3 +44,5 @@ def platinum_curfew_checker(current_time, curfew_time)
     return "You're in trouble! Better get back to Hogwarts quick!"
   end
 end
+
+simple_curfew_checker(11)
